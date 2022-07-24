dac816809ba61d2257a21906a89411926ebea92a
K Samarth N Kamath <kamathsamarth@gmail.com>
2022-07-24 14:55:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from scem_coin import app
 
 if __name__ == "__main__":
-    app.run(host='https://kamath-samarth-fyp.campuscrypto.zeet.app',port=3000)
+    app.run(host="127.0.0.1", port=3000, debug=True)
 
 
 
